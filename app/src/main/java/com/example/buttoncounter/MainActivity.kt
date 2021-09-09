0df6577a150dc902cb78ecbc5e0408178d96d706
Gaurav <sgaurav37533@gmail.com>
2021-09-10 02:44:46 +0530

    The Button Counter Message

--- app/src/main/java/com/example/buttoncounter/MainActivity.kt
+++ app/src/main/java/com/example/buttoncounter/MainActivity.kt
@@ -9,27 +9,21 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private var userInput: EditText? =null
-    private var button: Button? = null
     private var textView: TextView? = null
-    private var numTimesClicked =0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        userInput = findViewById<EditText>(R.id.editTextTextPersonName)
-        button = findViewById<Button>(R.id.button)
+        val userInput: EditText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val button: Button = findViewById<Button>(R.id.button)
         textView = findViewById<TextView>(R.id.textView)
         textView?.movementMethod =ScrollingMovementMethod()
 
-        button?.setOnClickListener(object : View.OnClickListener{
+        button.setOnClickListener(object : View.OnClickListener{
             override fun onClick(p0: View?) {
-                numTimesClicked +=1
-                textView?.append("The Button got Tapped $numTimesClicked time")
-                if(numTimesClicked!=1)
-                    textView?.append("s\n")
-                else
-                    textView?.append("\n")
+                textView?.append(userInput.text)
+                textView?.append("\n")
+
             }
         })
     }
